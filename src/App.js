b5f356a6c8dd630fb747f84e161e2294bbb1d1e5
tra38 <tra38@nau.edu>
2020-06-18 18:23:06 -0500

    Refactor duas into its own file and add more duas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import jsonArray from './duas/duas.json';
 
 //https://stackoverflow.com/a/11935263
 function getRandomSubarray(array, size)
 {
+  console.log("HELLO WORLD!");
+  console.log(array);
   var shuffled = array.slice(0);
   var i = array.length;
   var min = i - size;
@@ -21,39 +24,6 @@ function getRandomSubarray(array, size)
 
   return shuffled.slice(min);
 }
-
-var jsonArray = [
-    {
-      "quote": "Those who remember Allah (always, and in prayers) standing, sitting, and lying down on their sides, and think deeply about the creation of the heavens and the earth, (saying): 'Our Lord! You have not created (all) this without purpose, glory to You! (Exalted be You above all that they associate with You as partners). Give us salvation from the torment of the Fire.'",
-      "original_arabic": "الَّذِينَ يَذْكُرُونَ اللَّهَ قِيَامًا وَقُعُودًا وَعَلَىٰ جُنُوبِهِمْ وَيَتَفَكَّرُونَ فِي خَلْقِ السَّمَاوَاتِ وَالْأَرْضِ رَبَّنَا مَا خَلَقْتَ هَٰذَا بَاطِلًا سُبْحَانَكَ فَقِنَا عَذَابَ النَّارِ",
-      "source": "Quran 3:191 (Muhsin Khan translation)"
-    },
-    {
-      "quote": "Our Lord! Verily, whom You admit to the Fire, indeed, You have disgraced him, and never will the Zalimun (polytheists and wrong-doers) find any helpers.",
-      "original_arabic": "رَبَّنَا إِنَّكَ مَن تُدْخِلِ النَّارَ فَقَدْ أَخْزَيْتَهُ وَمَا لِلظَّالِمِينَ مِنْ أَنصَارٍ",
-      "source": "Quran 3:192 (Muhsin Khan translation)"
-    },
-    {
-      "quote": "Our Lord! Verily, we have heard the call of one (Muhammad SAW) calling to Faith: 'Believe in your Lord,' and we have believed. Our Lord! Forgive us our sins and remit from us our evil deeds, and make us die in the state of righteousness along with Al-Abrar (those who are obedient to Allah and follow strictly His Orders).",
-      "original_arabic": "رَّبَّنَا إِنَّنَا سَمِعْنَا مُنَادِيًا يُنَادِي لِلْإِيمَانِ أَنْ آمِنُوا بِرَبِّكُمْ فَآمَنَّا رَبَّنَا فَاغْفِرْ لَنَا ذُنُوبَنَا وَكَفِّرْ عَنَّا سَيِّئَاتِنَا وَتَوَفَّنَا مَعَ الْأَبْرَارِ",
-      "source": "Quran 3:193 (Muhsin Khan translation)"
-    },
-    {
-      "quote": "Our Lord! Grant us what You promised unto us through Your Messengers and disgrace us not on the Day of Resurrection, for You never break (Your) Promise.",
-      "original_arabic": "رَبَّنَا وَآتِنَا مَا وَعَدتَّنَا عَلَىٰ رُسُلِكَ وَلَا تُخْزِنَا يَوْمَ الْقِيَامَةِ إِنَّكَ لَا تُخْلِفُ الْمِيعَادَ",
-      "source": "Quran 3:194 (Muhsin Khan translation)"
-    },
-    {
-      "quote": "Who, when disaster strikes them, say, 'Indeed we belong to Allah, and indeed to Him we will return.'",
-      "original_arabic": "الَّذِينَ إِذَا أَصَابَتْهُم مُّصِيبَةٌ قَالُوا إِنَّا لِلَّهِ وَإِنَّا إِلَيْهِ رَاجِعُونَ",
-      "source": "Quran 2:156 (Muhsin Khan translation)"
-    },
-    {
-      "quote": "Indeed, I have turned my face towards He who created the heavens and the earth, inclining towards truth, and I am not of those who associate others with Allah.",
-      "original_arabic": "إِنِّي وَجَّهْتُ وَجْهِيَ لِلَّذِي فَطَرَ السَّمَاوَاتِ وَالْأَرْضَ حَنِيفًا وَمَا أَنَا مِنَ الْمُشْرِكِينَ",
-      "source": "Quran 6:79 (Sahih International translation)"
-    }
-];
 
 function citationFormatter(passage) {
     var licenseInfo, shortened_new_quote, shortened_original_quote, source_link;
@@ -175,8 +145,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-        key: Generator(jsonArray),
-        //key: Math.random( ),
+        key: Generator(jsonArray)
     };
   }
 
@@ -185,9 +154,6 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <ArabicText duaArray = {this.state.key } />
           <br />
           <EnglishTranslation duaArray = {this.state.key} />
