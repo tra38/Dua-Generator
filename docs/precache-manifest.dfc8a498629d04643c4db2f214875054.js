7e449d34ba699a9c503091b3f26bc190dd2702a1
tra38 <tra38@nau.edu>
2020-06-18 22:27:10 -0500

    Update docs folder

---
+++ docs/precache-manifest.dfc8a498629d04643c4db2f214875054.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913fd1c93a3b68797860d9e0809802a6",
+    "url": "/Dua-Generator/index.html"
+  },
+  {
+    "revision": "99038cf3e699bd0625f1",
+    "url": "/Dua-Generator/static/css/main.e6eaf654.chunk.css"
+  },
+  {
+    "revision": "b0029b6ba021fa2254a8",
+    "url": "/Dua-Generator/static/js/2.d80d215b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dua-Generator/static/js/2.d80d215b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99038cf3e699bd0625f1",
+    "url": "/Dua-Generator/static/js/main.6b868a07.chunk.js"
+  },
+  {
+    "revision": "46438e50833310afb64c",
+    "url": "/Dua-Generator/static/js/runtime-main.ce1c0e01.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Dua-Generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f52e26900a46b5ebdcf14c539d3788f.js"
+  "/Dua-Generator/precache-manifest.dfc8a498629d04643c4db2f214875054.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dua-Generator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
